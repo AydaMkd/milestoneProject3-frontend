5978944123f816236a568cbff56e15c7cec51b95
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2022-07-15 13:06:46 -0500

    Started SiteCards.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-
 import React from 'react';
+import ReactDOM from "react-dom/client";
 import './App.css';
 import HeaderBar from './components/HeaderBar';
-import SiteCards from './components/SiteCards';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+import LoginForm from './users/LoginForm'
 import SignUpForm from './users/SignUpForm';
+import CurrentUserProvider from './contexts/CurrentUser';
+import Navigation from './navigation';
+import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <header>
-          <HeaderBar />
-          <SiteCards />
-            
-        </header>
-      </Router>
-    </div>
+
+    <CurrentUserProvider>
+    <BrowserRouter>
+     
+      <Routes>
+      <Route path="/" element={<HeaderBar/>} />
+        <Route path="/sign-up" element={<SignUpForm/>} />
+        <Route path="/login" element={<LoginForm/>} />
+       
+       
+      </Routes>
+    </BrowserRouter>
+  </CurrentUserProvider>
+
   );
 }
 
