41399163bdd6c0534ea1a6bb6c13d6ce01108c66
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2022-07-14 20:26:19 -0500

    added about_js

---
+++ src/components/About.js
@@ -0,0 +1,27 @@
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+
+
+const About = () => {
+    return (
+        <Container>
+            <Card border="info" >
+                <Card.Header className="blockquote mb-0 card-body">About Us</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        We created this app to find and share recipes. This is our
+                    </Card.Text>
+                     <Card.Text>
+                        final Milestone project for a software developement bootcamp. 
+                    </Card.Text>
+                    <Card.Text>
+                        We hope you enjoy it! 
+                    </Card.Text>
+                </Card.Body>
+                <Card.Img style={{ width: '50%', margin: '0 auto'}}  variant="bottom" src="foodBackground.jpg" />
+            </Card>
+        </Container>
+    );
+}
+
+export default About;
