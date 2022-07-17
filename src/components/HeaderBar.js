10cfd7ff587f6da765f8e403536dd4731d375006
Christina <christina.hurt@purplewave.com>
2022-07-17 15:09:54 -0500

    change to headerbar

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -46,33 +46,13 @@ function HeaderBar() {
 
   return (
     <Navbar bg="light" expand="lg">
-      <Container fluid>
+      <Container>
         <Navbar.Brand href="#">Hangry Nomster</Navbar.Brand>
 
         {loginActions}
         <Navbar.Toggle aria-controls="navbarScroll" />
 
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          ></Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <NavDropdown title="Search by" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Cuisine</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Rating</NavDropdown.Item>
-              <NavDropdown.Item href="#action5">Difficulty</NavDropdown.Item>
-            </NavDropdown>
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
+       
       </Container>
     </Navbar>
   );
--- src/contexts/CurrentUser.js
+++ src/contexts/CurrentUser.js
@@ -6,21 +6,6 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
-<<<<<<< HEAD
-    
-    useEffect(() => {
-        const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile', {
-                headers: {
-                    'Authorization': localStorage.getItem('token')
-                }
-            })
-            let user = await response.json()
-            setCurrentUser(user)
-        }
-        getLoggedInUser()
-    }, [])
-=======
 
     // useEffect(() => {
     //     const getLoggedInUser = async () => {
@@ -34,7 +19,6 @@ function CurrentUserProvider({ children }) {
     //     }
     //     getLoggedInUser()
     // }, [])
->>>>>>> 06f88753a761324f7b957a93fe73abaf8eae58d2
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
