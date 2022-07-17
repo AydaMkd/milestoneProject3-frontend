8de0b563e12913b4e65662f1fe8b1cefe496728b
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-07-17 16:37:53 -0500

    just more stuff

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignUpForm from './users/SignUpForm';
 import CurrentUserProvider from './contexts/CurrentUser';
 import { BrowserRouter } from 'react-router-dom';
 import NewRecipe from './components/NewRecipe';
-import MyRecipes from './components/MyRecipes';
+import Recipes from './components/Recipes';
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
           <Route path="/sign-up" element={<SignUpForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/new" element={<NewRecipe />} />
-          <Route path="/recipes" element={<MyRecipes />} />
+          <Route path="/recipes" element={<Recipes />} />
         </Routes>
       </BrowserRouter>
     </CurrentUserProvider>
