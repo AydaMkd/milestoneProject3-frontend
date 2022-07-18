9843556853cdc05a7f4c128720eeb466e5df1009
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-07-18 00:05:20 -0500

    add more recipe fields

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -29,22 +29,44 @@ function Profile() {
         return <h1>Loading</h1>
     }
 
-    return (<>
+    
 
-        {myrecipes.map((myrecipe) => {
-            return (
-                <div className="col-sm-6" key={new Date(myrecipe.date).getUTCMilliseconds()}>
-                    <h2>
-                        {myrecipe.recipename}
-                    </h2>
-                    <p className="text-center">
-                        {myrecipe.description}
-                    </p>
-                    <img style={{ maxWidth: 200 }} src={myrecipe.image} alt={myrecipe.name} />
-                </div>
-            )
-        })
-        }</>)
+return (<>
+
+{myrecipes.map((myrecipe) => {
+    return (
+        <div className="col-sm-6" key={new Date(myrecipe.date).getUTCMilliseconds()}>
+            <h2>
+                {myrecipe.recipename}
+            </h2>
+            <img style={{ maxWidth: 200 }} src={myrecipe.image} alt={myrecipe.recipename} />
+            <p className="text-center">
+                {myrecipe.cuisines}
+            </p>
+            <p className="text-center">
+                {myrecipe.preptime}
+            </p>
+            <p className="text-center">
+                {myrecipe.cooktime}
+            </p>
+         
+            <p className="text-center">
+                {myrecipe.ingredients}
+            </p>
+            <p className="text-center">
+                {myrecipe.directions}
+            </p>
+            <p className="text-center">
+                {myrecipe.steps}
+            </p>
+            <p className="text-center">
+                {myrecipe.description}
+            </p>
+            
+        </div>
+    )
+})
+}</>)
 }
 
 export default Profile
