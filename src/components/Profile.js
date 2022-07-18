96995ba3bdddc101271c33db5eac829d728cb5d0
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-07-18 16:08:02 -0500

    make it better

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import { Image } from "react-bootstrap";
 
 function Profile() {
 
-    const [myrecipes, setMyrecipes] = useState(null)
+    const [myrecipes, setmyrecipes] = useState(null)
     // const resData = null
 
     useEffect(() => {
         console.log('useEffect')
         const fetchData = async () => {
-            const response = await fetch(`http://localhost:5000/api/recipes/myrecipes`, {
+            const response = await fetch(`http://localhost:5000/api/myrecipes`, {
 
                 headers: {
                     'x-auth-token': localStorage.getItem('token'),
@@ -18,7 +22,7 @@ function Profile() {
             const resData = await response.json()
             // if data is the same, don't update
             if (resData != myrecipes) {
-                setMyrecipes(resData)
+                setmyrecipes(resData)
             }
         }
         fetchData()
@@ -29,44 +33,42 @@ function Profile() {
         return <h1>Loading</h1>
     }
 
-    
+    return (<>
 
-return (<>
-
-{myrecipes.map((myrecipe) => {
-    return (
-        <div className="col-sm-6" key={new Date(myrecipe.date).getUTCMilliseconds()}>
-            <h2>
-                {myrecipe.recipename}
-            </h2>
-            <img style={{ maxWidth: 200 }} src={myrecipe.image} alt={myrecipe.recipename} />
-            <p className="text-center">
-                {myrecipe.cuisines}
-            </p>
-            <p className="text-center">
-                {myrecipe.preptime}
-            </p>
-            <p className="text-center">
-                {myrecipe.cooktime}
-            </p>
-         
-            <p className="text-center">
-                {myrecipe.ingredients}
-            </p>
-            <p className="text-center">
-                {myrecipe.directions}
-            </p>
-            <p className="text-center">
-                {myrecipe.steps}
-            </p>
-            <p className="text-center">
-                {myrecipe.description}
-            </p>
-            
-        </div>
-    )
-})
-}</>)
+        {myrecipes.map((recipe) => {
+            return (
+                <Container>
+                    <Card>
+                        <Accordion defaultActiveKey={new Date(recipe.date).getUTCMilliseconds()}>
+                            <Accordion.Item eventKey="0">
+                                <Accordion.Header>{recipe.recipename}</Accordion.Header>
+                                <Accordion.Body>
+                                    <div class="container my-5">
+                                        <div class="card row flex-row-reverse">
+                                            <img class="col-lg-4 card-img-end img-fluid p-0" src={recipe.image} />
+                                            <div class="col-lg-8 card-body">
+                                                <h4 class="card-title">{recipe.description}</h4>
+                                                <p class="card-text">
+                                                    <ul>
+                                                        <li><b>Cuisine:</b> {recipe.cuisines}</li>
+                                                        <li><b>Prep Time:</b> {recipe.preptime}</li>
+                                                        <li><b>Cook Time:</b> {recipe.cooktime}</li>
+                                                        <li><b>Ingredients:</b> {recipe.ingredients}</li>
+                                                        <li><b>Steps:</b> {recipe.steps}</li>
+                                                    </ul>
+                                                </p>
+                                                <p>{recipe.directions}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    </Card>
+                </Container>
+            )
+        })
+        }</>)
 }
 
 export default Profile
