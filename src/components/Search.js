56a17746543ab6ad0252ccfecba4aea6101efdc3
Ayda Mkaddem <92482497+AydaMkd@users.noreply.github.com>
2022-07-19 00:29:42 -0500

    added searchbar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,38 +1,65 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
+function Search() {
 
-const Search = (props) => {
-    let [searchTerm, setSearchTerm] = useState('')
+    const [menus, setMenus] = useState('')
+    const [query, setQuery] = useState('');
+    // const resData = null
+   
+    async function handleSearch(e) {
+        e.preventDefault()
+            const response = await fetch(`http://localhost:5000/api/recipes/search/${query}`, {
 
+                headers: {
+                    'x-auth-token': localStorage.getItem('token'),
+                    'Content-Type': 'application/json'
+                },
+            })
+            const resData = await response.json()
+            // if data is the same, don't update
+           
+                setMenus(resData)
+            
+        }
+    
+    console.log(menus)
+    console.log(query)
+    let display = (
+        <>
+         
+        </>
+      );
+    if (menus.length > 0) {display = (
+   <>
+   <ul>
+  {menus.map(menu => {
     return (
-            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-                <input type="text" placeholder="Search Here" onChange={(e) => setSearchTerm(e.target.value)} />
-                <input type="submit" />
-            </form>
+      <ul>
+      <li >
+        {menu.recipename}
+      </li>
+      <li >
+      <img width="200" height="200" src={menu.image} />
+      </li>
+      </ul>
+    )
+  })}
+</ul>
+
+   </>
     )
 }
 
-export default Search
+    return (
+        <div>
+            
+                <input type="text" placeholder="Search recipes by ingredients" onChange={(e) => setQuery(e.target.value)} value={query} />
+                <input type="submit" onClick={handleSearch} />
+               
+         {display}
+        </div>
+       
+    )
+}
 
-// //saving some code for the search feature
-// {/* <Navbar.Collapse id="navbarScroll">
-// <Nav
-//   className="me-auto my-2 my-lg-0"
-//   style={{ maxHeight: "100px" }}
-//   navbarScroll
-// ></Nav>
-// <Form className="d-flex">
-  
-//   <NavDropdown title="Search by" id="navbarScrollingDropdown">
-//     <NavDropdown.Item href="#action3">Cuisine</NavDropdown.Item>
-//     <NavDropdown.Item href="#action4">Rating</NavDropdown.Item>
-//     <NavDropdown.Item href="#action5">Difficulty</NavDropdown.Item>
-//   </NavDropdown>
-//   <Button variant="outline-success">Search</Button>
-//   <Form.Control
-//     type="search"
-//     placeholder="Search"
-//     className="me-2"
-//     aria-label="Search"
-//   />
-// </Form>
-// </Navbar.Collapse> */}
+export default Search;
