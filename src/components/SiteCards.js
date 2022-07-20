ea9d71f10c431043126e5538c672623bd3b4bf54
Christina <christina.hurt@purplewave.com>
2022-07-20 06:15:18 -0500

    approving ayda's changes

--- src/components/SiteCards.js
+++ src/components/SiteCards.js
@@ -86,3 +86,53 @@ function SiteCards() {
 
 
 export default SiteCards;
+
+// import { Link, Routes, Route } from 'react-router-dom';
+// import Card from 'react-bootstrap/Card';
+// import Nav from 'react-bootstrap/Nav';
+// import { useContext } from "react";
+// import { CurrentUser } from '../contexts/CurrentUser'
+
+// function SiteCards() {
+
+//     const { currentUser } = useContext(CurrentUser)
+
+//     return (
+//         <>{
+//             !currentUser ? <Card>
+//                 <Card.Header>
+//                     <Nav variant="tabs" defaultActiveKey="#first">
+//                         <Nav.Item>
+//                             <Nav.Link as={Link} to="/about">About</Nav.Link>
+//                         </Nav.Item>
+//                         <Nav.Item>
+//                             <Nav.Link as={Link} to="/recipes">Recipes</Nav.Link>
+//                         </Nav.Item>
+//                     </Nav>
+//                 </Card.Header>
+//             </Card>
+//                 :
+//                 <Card>
+//                     <Card.Header>
+//                         <Nav variant="tabs" defaultActiveKey="#first">
+//                             <Nav.Item>
+//                                 <Nav.Link as={Link} to="/about">About</Nav.Link>
+//                             </Nav.Item>
+//                             <Nav.Item>
+//                                 <Nav.Link as={Link} to="/recipes">Recipes</Nav.Link>
+//                             </Nav.Item>
+//                             <Nav.Item>
+//                                 <Nav.Link as={Link} to="/myRecipes">My Recipes</Nav.Link>
+//                             </Nav.Item>
+//                             <Nav.Item>
+//                                 <Nav.Link as={Link} to="/newRecipe">Add Recipe</Nav.Link>
+//                             </Nav.Item>
+//                         </Nav>
+//                     </Card.Header>
+//                 </Card>
+//         }
+//         </>
+//     )
+// }
+
+// export default SiteCards;
