b5a9af9e777684bfca9050514e80fdba40e32962
Christina <christina.hurt@purplewave.com>
2022-07-23 08:43:54 -0500

    deployment

--- src/components/show.js
+++ src/components/show.js
@@ -1,66 +1,66 @@
-import { useEffect, useState } from "react";
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import { Image } from "react-bootstrap";
+// import { useEffect, useState } from "react";
+// import Accordion from 'react-bootstrap/Accordion';
+// import Card from 'react-bootstrap/Card';
+// import Container from 'react-bootstrap/Container';
+// import { Image } from "react-bootstrap";
 
-function Profile() {
+// function Profile() {
 
-    const [myrecipes, setMyrecipes] = useState(null)
-    // const resData = null
+//     const [myrecipes, setMyrecipes] = useState(null)
+//     // const resData = null
 
-    useEffect(() => {
-        console.log('useEffect')
-        const fetchData = async () => {
-            // const response = await fetch(`https://hangrynomster.herokuapp.com/api/recipes/myrecipes`, {
-                const response = await fetch(`http://localhost:5000/api/recipes/myrecipes`, {
-                headers: {
-                    'x-auth-token': localStorage.getItem('token'),
-                    'Content-Type': 'application/json'
-                },
-            })
-            const resData = await response.json()
-            // if data is the same, don't update
-            if (resData != myrecipes) {
-                setMyrecipes(resData)
-            }
+//     useEffect(() => {
+//         console.log('useEffect')
+//         const fetchData = async () => {
+//             // const response = await fetch(`https://hangrynomster.herokuapp.com/api/recipes/myrecipes`, {
+//                 const response = await fetch(`http://localhost:5000/api/recipes/myrecipes`, {
+//                 headers: {
+//                     'x-auth-token': localStorage.getItem('token'),
+//                     'Content-Type': 'application/json'
+//                 },
+//             })
+//             const resData = await response.json()
+//             // if data is the same, don't update
+//             if (resData != myrecipes) {
+//                 setMyrecipes(resData)
+//             }
            
-        }
-        fetchData()
-    }, [])
-    console.log(myrecipes)
+//         }
+//         fetchData()
+//     }, [])
+//     console.log(myrecipes)
 
-    if (myrecipes === null) {
-        return <h1>Loading</h1>
-    }
+//     if (myrecipes === null) {
+//         return <h1>Loading</h1>
+//     }
 
-    return (<>
+//     return (<>
 
-        {myrecipes.map((myrecipe) => {
-            return (
-                <Container>
-                    <Card>
-                        <Accordion defaultActiveKey={new Date(myrecipe.date).getUTCMilliseconds()}>
-                            <Accordion.Item eventKey="0">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-                                <Accordion.Body>
-                                    <div class="container my-5">
+//         {myrecipes.map((myrecipe) => {
+//             return (
+//                 <Container>
+//                     <Card>
+//                         <Accordion defaultActiveKey={new Date(myrecipe.date).getUTCMilliseconds()}>
+//                             <Accordion.Item eventKey="0">
+//                             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+//         Accordion Item #1
+//       </button>
+//                                 <Accordion.Body>
+//                                     <div class="container my-5">
                                         
-                                                    <ul>
-                                                        <li><b>Cuisine:</b> {myrecipe.recipename}</li>
-                                                        </ul>
+//                                                     <ul>
+//                                                         <li><b>Cuisine:</b> {myrecipe.recipename}</li>
+//                                                         </ul>
                                       
-                                    </div>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                    </Card>
-                </Container>
-            )
-        })
-        }</>)
-}
+//                                     </div>
+//                                 </Accordion.Body>
+//                             </Accordion.Item>
+//                         </Accordion>
+//                     </Card>
+//                 </Container>
+//             )
+//         })
+//         }</>)
+// }
 
-export default Profile
+// export default Profile
